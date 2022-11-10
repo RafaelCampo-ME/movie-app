96a10bcaf1b85e0601e7cb481f663a86eccbd101
Rafael <rs.campo10@uniandes.edu.co>
2022-11-10 07:54:15 -0500

    Changes in new views method

--- movieapp/movieAPI/views.py
+++ movieapp/movieAPI/views.py
@@ -1,6 +1,9 @@
 from .models import  Movie
 from .serializer import MovieSerializer
 from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class MovieList(generics.ListAPIView):
@@ -10,4 +13,21 @@ class MovieList(generics.ListAPIView):
 class PostMovie(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+class createMovie(ModelViewSet):
+    queryset = Movie.objects.none()
+    serializer_class = MovieSerializer
+    http_method_names = ['post',]
+
+    def create(self, request, *arg, **kwargs):
+        user = request.user
+        data = {
+            "title":request.Movie.get('title',None)
+        }
+        serializer = self.serializer_class(data=data, contex = {'author':user})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
